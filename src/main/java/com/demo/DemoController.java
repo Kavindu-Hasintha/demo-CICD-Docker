75c4c1488ebfc1de5b35a6abcb56eca4ad5ecc2a
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-04-29 14:53:51 +0530

    init commit

---
+++ src/main/java/com/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> getDemo() {
+        String response = "Hello World! -> v1.0";
+        return ResponseEntity.ok(response);
+    }
+}
